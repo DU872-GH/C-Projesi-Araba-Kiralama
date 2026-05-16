058a4b494d4d535fa67dde521e13d6022eda1124
DU872-GH <denizunal872@gmail.com>
2026-05-16 16:06:35 +0300

    Add files via upload

--- Araba_Kiralama/Araba_Kiralama/Program.cs
+++ Araba_Kiralama/Araba_Kiralama/Program.cs
@@ -11,7 +11,7 @@ namespace araba_kiralama
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new btnArabaEkle());
+            Application.Run(new AnaMenu());
         }
     }
 }
